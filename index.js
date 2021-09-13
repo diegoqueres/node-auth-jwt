2a1dbf46c1b8dca4cd756d088300ad76498240a6
Diego Queres <diegoqueres81@gmail.com>
2021-09-13 20:21:01 -0300

    Autorização

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const express = require('express'); 
 const app = express(); 
 const jwt = require('jsonwebtoken');
+const blacklist = [];   //usar redis ou tabela, etc
  
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -12,7 +13,7 @@ app.get('/', (req, res, next) => {
     res.json({message: "Tudo ok por aqui!"});
 })
  
-app.get('/clientes', (req, res, next) => { 
+app.get('/clientes', verifyJWT, (req, res, next) => { 
     console.log("Retornou todos clientes!");
     res.json([{id:1,nome:'luiz'}]);
 }) 
@@ -22,8 +23,8 @@ app.post('/login', (req, res, next) => {
     //esse teste abaixo deve ser feito no seu banco de dados
     if(req.body.user === 'luiz' && req.body.password === '123'){
       //auth ok
-      const id = 1; //esse id viria do banco de dados
-      const token = jwt.sign({ id }, process.env.SECRET, {
+      const userId = 1; //esse id viria do banco de dados
+      const token = jwt.sign({ userId }, process.env.SECRET, {
         expiresIn: 300 // expires in 5min
       });
       return res.json({ auth: true, token: token });
@@ -32,13 +33,26 @@ app.post('/login', (req, res, next) => {
     res.status(500).json({message: 'Login inválido!'});
 })
 
-/*
-    Se quiser adicionar uma camada a mais de segurança, você pode ter uma blacklist de tokens que fizeram logout, para impedir reuso deles depois do logout realizado. Apenas lembre-se de limpar essa lista depois que eles expirarem ou use um cache em Redis com expiração automática.
-*/
-//logout
 app.post('/logout', function(req, res) {
-    res.json({ auth: false, token: null });
+    const token = req.headers['x-access-token']; 
+    blacklist.push(token);
+    res.json({ auth: false, token });
 })
+
+function verifyJWT(req, res, next){
+    const token = req.headers['x-access-token'];   
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    
+    if (blacklist.includes(token)) return res.status(401).json({ auth: false, message: 'User already logged out.' });
+    
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+      if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
+      
+      // se tudo estiver ok, salva no request para uso posterior
+      req.userId = decoded.userId;
+      next();
+    });
+}
 
 const server = http.createServer(app); 
 server.listen(3000);
